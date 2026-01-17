ab99af9083a4317f2655bb1bb37adeaafef3bdbc
aahektor <aronhektor7@gmail.com>
2026-01-17 17:09:32 +0100

    add: backend & create vite@latest

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,34 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader();
+                      });
+});
+
+//services
+builder.Services.AddControllers();
+
+
+
+string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentNullException("connectionstring is null");
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(connectionString));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(MyAllowSpecificOrigins);
+
+//middle wares
+app.MapControllers();
 app.Run();
 
--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce54d621c41befb2e8f43a06ae63c167d86fa9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
